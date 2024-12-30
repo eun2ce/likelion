4999142622e3cc4ea3c93cddd14c8e06906a2af3
eun2ce <joeun2ce@gmail.com>
2024-12-30 11:09:20 +0900

    feat: enum 에 값을 부여하는 방법

--- happyJava/src/Main.java
+++ happyJava/src/Main.java
@@ -1,54 +1,21 @@
-interface StorageService {
+enum Day {
+  MON(1), TUE(2), WED(3), THU(4), FRI(5), SAT(6), SUN(7);
 
-  void save(String data);
-}
+  private final int value;
 
-class FileStorage implements StorageService {
-
-  @Override
-  public void save(String data) {
-    System.out.println("데이터를 파일에 저장합니다. " + data);
-  }
-}
-
-class DatabaseStorage implements StorageService {
-
-  @Override
-  public void save(String data) {
-    System.out.println("데이터를 DB에 저장합니다. " + data);
-  }
-}
-
-class DataManager {
-
-  private final StorageService storageService;
-
-  public DataManager(StorageService storageService) {
-    this.storageService = storageService;
+  private Day(int value) {
+    this.value = value;
   }
 
-  public void saveData(String data) {
-    storageService.save(data);
+  public int getValue() {
+    return value;
   }
 }
 
 public class Main {
 
   public static void main(String[] args) {
-
-//        DataManager dataManager = new DataManager();
-//        dataManager.saveData("실습데이터입니다.");
-
-    //DIP 가 적용된 후에는 아래 코드로 실행될 수 있도록 코드를 수정해 주세요.
-
-    // 파일 저장 방식
-    StorageService fileStorage = new FileStorage();
-    DataManager fileDataManager = new DataManager(fileStorage);
-    fileDataManager.saveData("파일 저장 데이터");
-
-    // 데이터베이스 저장 방식
-    StorageService dbStorage = new DatabaseStorage();
-    DataManager dbDataManager = new DataManager(dbStorage);
-    dbDataManager.saveData("데이터베이스 저장 데이터");
+    Day d = Day.MON;
+    System.out.println(d.getValue());
   }
 }
