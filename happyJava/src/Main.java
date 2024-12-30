b2ed8a40ce8f3258356754ff4ec150f966a535f4
eun2ce <joeun2ce@gmail.com>
2024-12-30 11:37:29 +0900

    feat: stream 을 이용한 enum

--- happyJava/src/Main.java
+++ happyJava/src/Main.java
@@ -1,21 +1,40 @@
-enum Day {
-  MON(1), TUE(2), WED(3), THU(4), FRI(5), SAT(6), SUN(7);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+enum Menu {
+  ONE(1), TWO(2), THREE(3);
 
   private final int value;
 
-  private Day(int value) {
+  Menu(int value) {
     this.value = value;
   }
 
-  public int getValue() {
-    return value;
+  public static Menu findByValue(int value) {
+    return Arrays.stream(values()).filter(menu -> (menu.value == value)).findFirst()
+        .orElseThrow(IllegalAccessError::new);
   }
 }
 
 public class Main {
 
-  public static void main(String[] args) {
-    Day d = Day.MON;
-    System.out.println(d.getValue());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    Menu menu = Menu.findByValue(Integer.parseInt(br.readLine()));
+
+    switch (menu) {
+      case ONE:
+        System.out.println("one");
+        break;
+      case TWO:
+        System.out.println("two");
+        break;
+      case THREE:
+        System.out.println("three");
+        break;
+    }
   }
 }
