3b1a3f4dc11889028ec779842b1e240a9ebd08d8
eun2ce <joeun2ce@gmail.com>
2025-01-17 16:50:52 +0900

    feat: user service

--- iocexam/src/main/java/com/example/iocexam/IocexamApplication.java
+++ iocexam/src/main/java/com/example/iocexam/IocexamApplication.java
@@ -2,7 +2,6 @@ package com.example.iocexam;
 
 import com.example.iocexam.config.UserConfig;
 import com.example.iocexam.controller.UserController;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,7 +12,7 @@ public class IocexamApplication {
     ApplicationContext context = new AnnotationConfigApplicationContext(UserConfig.class);
 
     UserController controller = context.getBean(UserController.class);
-    controller.joinUser("eunhee", "joeun2ce@gmail.com", "changeme");
+    controller.joinUser("eun2ce", "joeun2ce@gmail.com", "changeme");
 
     //이렇게 실행하면~~~    user+"의 정보가 잘 저장되었습니다."  이렇게 출력될 수 있도록!!!   Bean을 등록해 보세요.
   }
