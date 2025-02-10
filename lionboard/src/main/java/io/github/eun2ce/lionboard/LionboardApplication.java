1a8b1152e96354176165a219e519ee8de6aed35c
eun2ce <joeun2ce@gmail.com>
2025-02-10 14:05:51 +0900

    [feat]: lion board skel code

---
+++ lionboard/src/main/java/io/github/eun2ce/lionboard/LionboardApplication.java
@@ -0,0 +1,13 @@
+package io.github.eun2ce.lionboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LionboardApplication.class, args);
+	}
+
+}
---
+++ lionboard/src/test/java/io/github/eun2ce/lionboard/LionboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eun2ce.lionboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
